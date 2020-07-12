7eb933917825dcbd7956c42c4a3fbc9e43536f86
Nils Hoffmann <the.nilshoffmann@gmail.com>
2020-07-12 12:28:00 +0200

    Cleaned up the project

---
+++ src/main/java/dev/nhoffmann/vaadin/crm/ui/MainView.java
@@ -0,0 +1,14 @@
+package dev.nhoffmann.vaadin.crm.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout
+{
+
+    public MainView()
+    {
+    }
+
+}
