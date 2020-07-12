6f55ef3eea4f1f2d17c4d1f31375cd7cfd640c8a
Nils Hoffmann <the.nilshoffmann@gmail.com>
2020-07-12 12:59:20 +0200

    Created a Grid with data, according to Chapter 4

--- src/main/java/dev/nhoffmann/vaadin/crm/ui/MainView.java
+++ src/main/java/dev/nhoffmann/vaadin/crm/ui/MainView.java
@@ -1,14 +1,46 @@
 package dev.nhoffmann.vaadin.crm.ui;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+
+import dev.nhoffmann.vaadin.crm.backend.entity.Company;
+import dev.nhoffmann.vaadin.crm.backend.entity.Contact;
+import dev.nhoffmann.vaadin.crm.backend.service.ContactService;
 
 @Route
 public class MainView extends VerticalLayout
 {
+    private final ContactService contactService;
+    private final Grid<Contact> grid = new Grid<>(Contact.class);
 
-    public MainView()
+    public MainView(final ContactService pContactService)
     {
+        contactService = pContactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+
+        add(grid);
+        updateGrid();
     }
 
+    private void configureGrid()
+    {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void updateGrid()
+    {
+        grid.setItems(contactService.findAll());
+    }
 }
